f90b11c1a8f3f4d3ad381846c2de7fd9913a884a
Andrew Bruce <andrewbruce.himni@gmail.com>
2024-03-23 23:03:45 -0700

    icd 10 validator, pkgdown

--- R/checks.R
+++ R/checks.R
@@ -1,3 +1,33 @@
+#' Check if string is valid ICD-10-CM code
+#' @param x string
+#' @param arg string
+#' @param call string
+#' @return boolean
+#' @examples
+#' purrr::map_vec(
+#' c("H00.019", "D50.1", "C4A.70", "Z20.818", "11646", "E8015"),
+#' is_valid_icd)
+#'
+#' @export
+#' @autoglobal
+is_valid_icd <- function(x,
+                            arg = rlang::caller_arg(x),
+                            call = rlang::caller_env()) {
+
+  if (stringr::str_length(x) < 3L || stringr::str_length(x) > 7L) {
+    cli::cli_abort(c(
+      "An {.strong ICD-10-CM} code is between {.emph 3-7} characters.",
+      "x" = "{.strong {.val {x}}} is {.val {nchar(x)}}."),
+      call = call)}
+
+  # https://www.johndcook.com/blog/2019/05/05/regex_icd_codes/
+  icd10_regex <- "[A-TV-Z][0-9][0-9AB]\\.?[0-9A-TV-Z]{0,4}"
+
+  stringr::str_detect(x, stringr::regex(icd10_regex))
+
+  if (grepl("[[:lower:]]*", x)) {toupper(x)}
+}
+
 #' Check if string is valid length of a HCPCS code
 #' @param x string
 #' @param arg string
@@ -112,8 +142,6 @@ is_level_II <- function(x,
 is_category_I <- function(x,
                           arg = rlang::caller_arg(x),
                           call = rlang::caller_env()) {
-
-  # https://www.johndcook.com/blog/2019/05/05/regex_icd_codes/
   is_level_I(x)
 
   stringr::str_detect(x, stringr::regex("^\\d{4}[A-EG-SU-Z0-9]$"))
